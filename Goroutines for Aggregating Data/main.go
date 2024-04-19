1c66c9cf42076406d9ddbb66f49086dcd76ae2a8
Kushagra102 <kushagra.garg77@gmail.com>
2024-04-19 16:20:42 +0530

    Goroutines for Aggregating Data

--- Goroutines for Aggregating Data/main.go
+++ Goroutines for Aggregating Data/main.go
@@ -1,1 +1,49 @@
+// Reducing req time from 350ms to 250ms by using goroutines for fetching user likes and matches concurrently and aggregating the data using channels.
 
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	userName := fetchUser()
+	respch := make(chan any, 2)
+	wg := &sync.WaitGroup{}
+
+	wg.Add(2)
+
+	go fetchUserLikes(userName, respch, wg)
+	go fetchUserMatch(userName, respch, wg)
+	wg.Wait()
+	close(respch)
+
+	for resp := range respch {
+		fmt.Println("Response:", resp)
+	}
+
+	fmt.Println("User:", userName)
+	fmt.Println("Took:", time.Since(start))
+}
+
+func fetchUser() string {
+	time.Sleep(time.Millisecond * 100)
+	return "KUSH"
+}
+
+func fetchUserLikes(userName string, respch chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 150)
+
+	respch <- 11
+	wg.Done()
+}
+
+func fetchUserMatch(userName string, respch chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 100)
+
+	respch <- "ANNA"
+	wg.Done()
+}
